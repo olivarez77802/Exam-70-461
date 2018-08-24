e8aba3e086e6599bc86eaff8324b1c17288c30d3
Jesse Olivarez <olivarez77802@gmail.com>
2018-08-24 06:19:15 -0500

    CreateDatabseObjects

---
+++ CreateDatabaseObjects/DesignViews.sql
@@ -0,0 +1,7 @@
+/*
+   Design Views
+   - Ensure code non regression by keeping consistent signature for procedure, views and functions (interfaces);
+     security implications.
+
+	 https://www.reddit.com/r/SQLServer/comments/1ipghq/exam_70461_question_code_non_regression_by/
+*/
