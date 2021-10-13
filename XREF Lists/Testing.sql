b501721c4c87b7bfc9c18bbe33d59d1bd6fe54a9
Olivarez <jesse-olivarez@tamus.edu>
2021-10-13 10:10:35 -0500

    October 2021 updates

---
+++ XREF Lists/Testing.sql
@@ -0,0 +1,18 @@
+/*
+Unit Testing
+
+https://tsqlt.org/
+TSQLT, THE OPEN SOURCE DATABASE UNIT TESTING FRAMEWORK FOR SQL SERVER
+
+Notes from Pluralsight SQL Unit Testing
+https://app.pluralsight.com/library/courses/unit-testing-t-sql-tsqlt/table-of-contents
+
+TSQL is installed into each database.  I installed using redgate into so-edw-sql2dev on 09/01/2001.
+
+SELECT * FROM tSQLt.Info()
+
+
+*/
+-- Gives version (so-edw-sql2dev)
+USE JesseTest 
+SELECT * FROM tSQLt.Info()
